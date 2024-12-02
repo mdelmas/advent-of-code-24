fd9c2cac7b13c7957b3c8ad7f79275c4b1a66c13
Morgane Delmas <mdelmas93@gmail.com>
2024-12-02 20:53:32 +0100

    day 2 part 2 wip

--- day2/day2.ts
+++ day2/day2.ts
@@ -6,9 +6,16 @@ enum Order {
 }
 
 const reports = fs
-  .readFileSync("./test", "utf-8")
+  .readFileSync("./input", "utf-8")
   .split("\n")
   .map((line) => line.split(" ").map((value) => +value));
+
+const checkOrder = (value1: number, value2: number, order: Order) => {
+  if (order === Order.INCREASING) {
+    return value2 > value1 && value2 <= value1 + 3;
+  }
+  return value2 < value1 && value2 >= value1 - 3;
+};
 
 // Part 1
 let countSafe = 0;
@@ -17,14 +24,37 @@ reports.forEach((report) => {
   const order = report[0] > report[1] ? Order.DECREASING : Order.INCREASING;
   const safe = report.every((_, i) => {
     if (i === 0) return true;
-
-    if (order === Order.INCREASING) {
-      return report[i] > report[i - 1] && report[i] <= report[i - 1] + 3;
-    }
-    return report[i] < report[i - 1] && report[i] >= report[i - 1] - 3;
+    return checkOrder(report[i - 1], report[i], order);
   });
 
   if (safe) countSafe++;
 });
 
 console.log("Part 1:", countSafe);
+
+// Part 2
+countSafe = 0;
+
+reports.forEach((report) => {
+  const order = report[0] > report[1] ? Order.DECREASING : Order.INCREASING;
+
+  let i = 1,
+    safe = true;
+  while (i < report.length) {
+    if (!checkOrder(report[i - 1], report[i], order)) {
+      if (
+        i + 1 < report.length &&
+        !checkOrder(report[i - 1], report[i + 1], order)
+      ) {
+        safe = false;
+        break;
+      }
+      i++;
+    }
+    i++;
+  }
+
+  if (safe) countSafe++;
+});
+
+console.log("Part 2:", countSafe);
